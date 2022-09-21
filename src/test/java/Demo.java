92f34d23972e1dd12b3fa0faf9be4cd45a2e2ca1
mckimssongg <soymckimsson286@gmail.com>
2022-09-20 18:23:48 -0600

    first commit

---
+++ src/test/java/Demo.java
@@ -0,0 +1,13 @@
+import org.testng.annotations.Test;
+
+public class Demo {
+
+    @Test
+    public void firstTestExample(){
+        System.out.println("este es un test");
+    }
+    @Test
+    public void secondTestExample(){
+        System.out.println("este es otro test");
+    }
+}
