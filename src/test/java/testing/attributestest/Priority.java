d129894e4be674523fee69956341e4dde65c96ad
mckimssongg <soymckimsson286@gmail.com>
2022-09-20 20:00:07 -0600

    atributos de Test

---
+++ src/test/java/testing/attributestest/Priority.java
@@ -0,0 +1,18 @@
+package testing.attributestest;
+
+import org.testng.annotations.Test;
+
+public class Priority {
+    @Test(priority = 3)
+    public void testCases1(){
+        System.out.println("Test 1");
+    }
+    @Test(dependsOnMethods = "testCases1")
+    public void testCases2(){
+        System.out.println("Test 2");
+    }
+    @Test(priority = 1)
+    public void testCases3(){
+        System.out.println("Test 3");
+    }
+}
